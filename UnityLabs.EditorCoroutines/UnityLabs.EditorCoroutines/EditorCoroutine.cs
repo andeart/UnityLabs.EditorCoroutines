4244118f41d417beca89f2e1954a98656acb9851
Anurag Devanapally <anurag.devanapally@gmail.com>
2019-02-12 18:59:56 -0800

    Add initial implementation

---
+++ UnityLabs.EditorCoroutines/UnityLabs.EditorCoroutines/EditorCoroutine.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections;
+using System.Reflection;
+using UnityEngine;
+
+
+namespace Andeart.UnityLabs.EditorCoroutines
+{
+
+    /// <summary>
+    /// <see cref="EditorCoroutineService.StartCoroutine(IEnumerator)"/> returns an EditorCoroutine.
+    /// Instances of this class are only used to reference these coroutines, and do not hold any exposed properties or functions.
+    /// 
+    /// An EditorCoroutine is a function that can suspend its execution (yield) until the given <see cref="YieldInstruction"/> finishes.
+    /// </summary>
+    public partial class EditorCoroutine
+    {
+        internal IYield CurrentYield { get; set; }
+        internal bool Finished { get; set; }
+
+        internal IEnumerator Routine { get; }
+        internal int OwnerHash { get; }
+        internal string Id { get; }
+
+        private EditorCoroutine () { }
+
+        internal EditorCoroutine (int ownerHash, IEnumerator routine)
+        {
+            CurrentYield = new Yield.Default ();
+            Finished = false;
+
+            Routine = routine ?? throw new ArgumentNullException (nameof(routine), "Routine used to start an EditorCoroutine cannot be null.");
+            OwnerHash = ownerHash;
+            Id = EditorCoroutineFactory.GetId (OwnerHash, routine);
+        }
+
+        internal void Evaluate ()
+        {
+            object current = Routine.Current;
+            switch (current)
+            {
+                case null:
+                case WaitForFixedUpdate _:
+                case WaitForEndOfFrame _:
+                    CurrentYield = new Yield.WaitForFrames (1);
+                    break;
+                case WaitForSeconds waitForSeconds:
+                    const BindingFlags bindingFlags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static;
+                    FieldInfo field = typeof(WaitForSeconds).GetField ("m_Seconds", bindingFlags);
+                    float seconds = float.Parse (field.GetValue (waitForSeconds).ToString ());
+                    CurrentYield = new Yield.WaitForSeconds (seconds);
+                    break;
+                case AsyncOperation asyncOperation:
+                    CurrentYield = new Yield.AsyncOperation (asyncOperation);
+                    break;
+                case CustomYieldInstruction customYieldInstruction:
+                    CurrentYield = new Yield.CustomYieldInstruction (customYieldInstruction);
+                    break;
+                case EditorCoroutine coroutine:
+                    CurrentYield = new Yield.NestedCoroutine (coroutine);
+                    break;
+                default:
+                    CurrentYield = new Yield.Default ();
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Determines whether the specified object is the same as this EditorCoroutine.
+        /// </summary>
+        public override bool Equals (object obj)
+        {
+            if (obj is EditorCoroutine coroutine)
+            {
+                return Id == coroutine.Id;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// A hash code for this EditorCoroutine.
+        /// </summary>
+        public override int GetHashCode ()
+        {
+            return Id.GetHashCode ();
+        }
+
+        /// <summary>
+        /// A string that represents this EditorCoroutine.
+        /// </summary>
+        public override string ToString ()
+        {
+            return Id;
+        }
+    }
+
+}
---
+++ UnityLabs.EditorCoroutines/UnityLabs.EditorCoroutines/EditorCoroutineService.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections;
+using System.Reflection;
+using UnityEditor;
+
+
+namespace Andeart.UnityLabs.EditorCoroutines
+{
+
+    /// <summary>
+    /// Static service to help start EditorCoroutines.
+    /// </summary>
+    public static class EditorCoroutineService
+    {
+        /// <summary>
+        /// Starts a coroutine named
+        /// <param name="methodName" />
+        /// .
+        /// In most cases you want to use the <seealso cref="StartCoroutine(IEnumerator)" /> variation.
+        /// You can <seealso cref="StopCoroutine(EditorWindow, string)" /> regardless of what approach/overload you used to start
+        /// it.
+        /// </summary>
+        /// <param name="owner">The EditorWindow to be set as the owner of this EditorCoroutine.</param>
+        public static EditorCoroutine StartCoroutine (this EditorWindow owner, string methodName)
+        {
+            return StartCoroutine (owner, methodName, null);
+        }
+
+        /// <summary>
+        /// Starts a coroutine named
+        /// <param name="methodName" />
+        /// .
+        /// In most cases you want to use the <seealso cref="StartCoroutine(IEnumerator)" /> variation.
+        /// You can <seealso cref="StopCoroutine(EditorWindow, string)" /> regardless of what approach/overload you used to start
+        /// it.
+        /// </summary>
+        /// <param name="owner">The EditorWindow to be set as the owner of this EditorCoroutine.</param>
+        /// <param name="methodArgs">The parameters to pass to the method being invoked.</param>
+        public static EditorCoroutine StartCoroutine (this EditorWindow owner, string methodName, params object[] methodArgs)
+        {
+            if (owner == null)
+            {
+                throw new ArgumentNullException (nameof(owner), "EditorCoroutine cannot be invoked by name from a null object.");
+            }
+
+            MethodInfo methodInfo = owner.GetType ().GetMethod (methodName, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+
+            if (methodInfo == null)
+            {
+                throw new ArgumentException ($"Coroutine with name {methodName} does not exist on instance of type {owner.GetType ()}.", nameof(methodName));
+            }
+
+            if (methodInfo.ReturnType != typeof(IEnumerator))
+            {
+                throw new ArgumentException ($"Coroutine with name {methodName} does not return an IEnumerator.", nameof(methodName));
+            }
+
+            object returned = methodInfo.Invoke (owner, methodArgs);
+            IEnumerator routine = returned as IEnumerator;
+            return StartCoroutine (owner, routine);
+        }
+
+        /// <summary>
+        /// Starts an EditorCoroutine.
+        /// The execution of a coroutine can be paused at any point using the yield statement.
+        /// When a yield statement is used, the coroutine will pause execution and automatically resume at the next frame.
+        /// See the <see cref="UnityEngine.Coroutine" /> documentation for more details.
+        /// Coroutines are excellent when modeling behavior over several frames. Coroutines have virtually no performance overhead.
+        /// StartCoroutine function always returns immediately, however you can yield the result. Yielding waits until the
+        /// coroutine has finished execution.
+        /// There is no guarantee that coroutines end in the same order that they were started, even if they finish in the same
+        /// frame.
+        /// You can <seealso cref="StopCoroutine(EditorWindow, string)" /> regardless of what approach/overload you used to start
+        /// it.
+        /// </summary>
+        public static EditorCoroutine StartCoroutine (IEnumerator routine)
+        {
+            return StartCoroutine (null, routine);
+        }
+
+        /// <summary>
+        /// Starts an EditorCoroutine.
+        /// The execution of a coroutine can be paused at any point using the yield statement.
+        /// When a yield statement is used, the coroutine will pause execution and automatically resume at the next frame.
+        /// See the <see cref="UnityEngine.Coroutine" /> documentation for more details.
+        /// Coroutines are excellent when modeling behavior over several frames. Coroutines have virtually no performance overhead.
+        /// StartCoroutine function always returns immediately, however you can yield the result. Yielding waits until the
+        /// coroutine has finished execution.
+        /// There is no guarantee that coroutines end in the same order that they were started, even if they finish in the same
+        /// frame.
+        /// You can <seealso cref="StopCoroutine(EditorWindow, string)" /> regardless of what approach/overload you used to start
+        /// it.
+        /// </summary>
+        public static EditorCoroutine StartCoroutine (this EditorWindow owner, IEnumerator routine)
+        {
+            EditorCoroutine coroutine = EditorCoroutineFactory.Create (owner, routine);
+            return EditorCoroutineUpdateService.Instance.StartCoroutine (coroutine);
+        }
+
+        /// <summary>
+        /// Stops all running EditorCoroutine instances named
+        /// <param name="methodName" />
+        /// and belonging to the type of
+        /// <param name="owner" />
+        /// .
+        /// </summary>
+        public static void StopCoroutine (this EditorWindow owner, string methodName)
+        {
+            string coroutineId = EditorCoroutineFactory.GetId (owner, methodName);
+            EditorCoroutineUpdateService.Instance.StopCoroutine (coroutineId);
+        }
+
+        /// <summary>
+        /// Stops all running EditorCoroutine instances that were returned by
+        /// <param name="routine" />
+        /// .
+        /// </summary>
+        public static void StopCoroutine (IEnumerator routine)
+        {
+            StopCoroutine (null, routine);
+        }
+
+        // TODO: StopCoroutine with an EditorCoroutine instance.
+
+        /// <summary>
+        /// Stops all running EditorCoroutine instances that were returned by
+        /// <param name="routine" />
+        /// and belonging to the type of
+        /// <param name="owner" />
+        /// .
+        /// </summary>
+        public static void StopCoroutine (this EditorWindow owner, IEnumerator routine)
+        {
+            string coroutineId = EditorCoroutineFactory.GetId (owner, routine);
+            EditorCoroutineUpdateService.Instance.StopCoroutine (coroutineId);
+        }
+
+        /// <summary>
+        /// Stops all running EditorCoroutine instances that belong to the type of
+        /// <param name="owner" />
+        /// .
+        /// </summary>
+        public static void StopAllCoroutines (this EditorWindow owner)
+        {
+            int ownerHash = EditorCoroutineFactory.GetOwnerHash (owner);
+            EditorCoroutineUpdateService.Instance.StopAllCoroutines (ownerHash);
+        }
+
+        /// <summary>
+        /// Stops all running EditorCoroutine instances. This is a Global "kill-all" mechanism.
+        /// </summary>
+        public static void StopAllCoroutines ()
+        {
+            EditorCoroutineUpdateService.Instance.StopAllCoroutines ();
+        }
+    }
+
+}
