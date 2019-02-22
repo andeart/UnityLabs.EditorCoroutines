59dea4dce2299c7a81d30627533378788f961fa6
Anurag D <andymations.ad@gmail.com>
2019-02-21 23:18:34 -0800

    Add post-build events to to push Release DLLs to Unity folders

---
+++ Scripts/push-to-unity-dir.bat
@@ -0,0 +1,25 @@
+set configurationName=%1
+set targetDir=%2
+set targetName=%3
+set targetCopyDir=%4
+
+if "%configurationName%"=="Release" (
+    cd /d %targetCopyDir%
+    for %%a in (*.dll*) do (
+        del "%%a"
+    )
+    for %%a in (*.pdb*) do (
+        del "%%a"
+    )
+    for %%a in (*.xml*) do (
+        del "%%a"
+    )
+    for %%a in (*.mdb*) do (
+        del "%%a"
+    )
+    
+    cd /d %targetDir%
+    copy "%targetName%.dll" %targetCopyDir% /y
+    copy "%targetName%.pdb" %targetCopyDir% /y
+    copy "%targetName%.xml" %targetCopyDir% /y
+)
