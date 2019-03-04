a7d844d9a69af2af5af4662be8fdf5dfbd19432d
andeart <andymations.ad@gmail.com>
2019-03-03 20:05:12 -0800

    Update assembly info

--- EditorCoroutines/EditorCoroutines/Properties/AssemblyInfo.cs
+++ EditorCoroutines/EditorCoroutines/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle ("Andeart.EditorCoroutines")]
-[assembly: AssemblyDescription ("EditorCoroutines allows you to start and stop Unity coroutines in Editor scripts.")]
+[assembly: AssemblyDescription ("EditorCoroutines allow you to start/stop Unity coroutines in Editor scripts, without forcing you to be in Play mode.")]
 [assembly: AssemblyConfiguration ("")]
 [assembly: AssemblyCompany ("Andeart")]
 [assembly: AssemblyProduct ("Andeart.EditorCoroutines")]
@@ -23,17 +23,6 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid ("d9082f31-71d6-4453-b178-a80ee10958b1")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion ("1.2.31.0")]
 [assembly: AssemblyFileVersion ("1.2.31.0")]
-[assembly:
-    InternalsVisibleTo ("Andeart.EditorCoroutines.Unity")]
+[assembly: InternalsVisibleTo ("Andeart.EditorCoroutines.Unity")]
